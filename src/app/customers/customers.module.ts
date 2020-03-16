02ac0e77b90e315cf21cb5084a3c1d886b7a111d
Jon Tran <trungt8998@gmail.com>
2020-03-16 12:18:45 -0500

    Create customer-list component under customers component and add to customers module

--- src/app/customers/customers.module.ts
+++ src/app/customers/customers.module.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { CustomersComponent } from './customers.component';
+import { CustomersListComponent } from './customers-list/customers-list.component';
 
 @NgModule({
-    declarations: [CustomersComponent],
+    declarations: [CustomersComponent, CustomersListComponent],
     imports: [CommonModule],
     exports: [CustomersComponent],
 })
