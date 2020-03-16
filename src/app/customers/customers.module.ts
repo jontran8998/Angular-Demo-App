b69a7a291fcdee9ea51e774edefe8a5ceff36dd3
Jon Tran <trungt8998@gmail.com>
2020-03-16 09:54:22 -0500

    Create new customers component and customers Module

---
+++ src/app/customers/customers.module.ts
@@ -0,0 +1,10 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { CustomersComponent } from './customers.component';
+
+@NgModule({
+    declarations: [CustomersComponent],
+    imports: [CommonModule],
+})
+
+export class CustomersModule {}
