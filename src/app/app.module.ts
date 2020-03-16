d53e195e4e504a7b8a5632593e8c1910bc106921
Jon Tran <trungt8998@gmail.com>
2020-03-16 14:09:28 -0500

    Create interface as model in db

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CustomersModule } from './customers/customers.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { CustomersModule } from './customers/customers.module';
     BrowserModule,
     AppRoutingModule,
     CustomersModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
