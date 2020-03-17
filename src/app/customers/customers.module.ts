30aad8c10e094f3f5f041be5314fad2d2c933d87
Jon Tran <trungt8998@gmail.com>
2020-03-17 09:10:32 -0500

    Using ngModel in FomsModule, set up Output, Apply Filter function

--- src/app/customers/customers.module.ts
+++ src/app/customers/customers.module.ts
@@ -4,10 +4,11 @@ import { CustomersComponent } from './customers.component';
 import { CustomersListComponent } from './customers-list/customers-list.component';
 import { FilterTextboxComponent } from './filter-textbox/filter-textbox.component';
 import { SharedModule } from '../shared/shared.module';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
     declarations: [CustomersComponent, CustomersListComponent, FilterTextboxComponent],
-    imports: [CommonModule, SharedModule],
+    imports: [CommonModule, SharedModule, FormsModule],
     exports: [CustomersComponent],
 })
 
