ac391f9c5b31d6c34bb3a96abe2d0adebf4fb3db
Jon Tran <trungt8998@gmail.com>
2020-03-17 13:51:59 -0500

    Add functions on data service

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
+import { DataService } from './data.service';
+import { SorterService } from './sorter.service';
 
 @NgModule({
     imports: [],
-    providers: []
+    providers: [ DataService, SorterService ]
 })
 export class CoreModule {}
