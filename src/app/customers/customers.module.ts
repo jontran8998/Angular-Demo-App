4224d2b8d6c1745ddba85f9fcac17282c2d5dddd
Jon Tran <trungt8998@gmail.com>
2020-03-17 18:38:11 -0500

    Set up customers routing

--- src/app/customers/customers.module.ts
+++ src/app/customers/customers.module.ts
@@ -5,10 +5,11 @@ import { CustomersListComponent } from './customers-list/customers-list.componen
 import { FilterTextboxComponent } from './filter-textbox/filter-textbox.component';
 import { SharedModule } from '../shared/shared.module';
 import { FormsModule } from '@angular/forms';
+import { CustomersRoutingModule } from './customers-routing.module';
 
 @NgModule({
     declarations: [CustomersComponent, CustomersListComponent, FilterTextboxComponent],
-    imports: [CommonModule, SharedModule, FormsModule],
+    imports: [CommonModule, SharedModule, FormsModule, CustomersRoutingModule],
     exports: [CustomersComponent],
 })
 
