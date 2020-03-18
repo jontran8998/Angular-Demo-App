952219fcba547f7d723f44a98147133c385f70cc
Jon Tran <trungt8998@gmail.com>
2020-03-17 23:16:46 -0500

    Setting stylesheet

--- src/app/customers/filter-textbox/filter-textbox.component.ts
+++ src/app/customers/filter-textbox/filter-textbox.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 @Component({
   selector: 'app-filter-textbox',
   template: `
-    Filter: <input type="text" [(ngModel)]="filter"/>
+    Filter: <input type="text" [(ngModel)]="filter" placeholder="Name/City/Total"/>
   `,
   styleUrls: ['./filter-textbox.component.css']
 })
